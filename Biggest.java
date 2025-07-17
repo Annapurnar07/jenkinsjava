b2099bdd2a99b8e78f4892f5eec6e468f142d2b7
Annapurna <147121038+Annapurnar07@users.noreply.github.com>
2025-07-17 15:20:57 +0530

    Create Biggest.java

---
+++ Biggest.java
@@ -0,0 +1,21 @@
+class Biggest
+{
+public static void main(Sring args[0])
+{
+int n1=integer.parseInt(args[0]);
+int n1=integer.parseInt(args[1]);
+int n1=integer.parseInt(args[2]);
+if((n1>n2)&&(n1>n3))
+{
+system.out.println(n1 +"is biggest");
+}
+else(if (n2>n1)&&(n2>n3))
+{
+system.out.println(n2 +"is biggest");
+}
+else
+{
+system.out.println(n3 +"is biggest");
+}
+}
+}
